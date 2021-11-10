c9366a60aa0d0447395c8af8b6f2bcb4acb72ec0
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-10 23:30:23 +0530

    added random fortunes

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const fortunes = require("./data/fortunes");
 
 app.get("/fortunes", (req, res)=>{
     res.json(fortunes);
-})
+});
+
+app.get("/fortunes/random", (req, res)=>{
+    res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get("/fortunes/:id", (req, res)=>{
+    res.json(fortunes.find(fortune => fortune.id == req.params.id));
+});
 
 module.exports = app;
