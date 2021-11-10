c408b017ae260e955c516100400c3bab3b2b35dd
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-10 23:11:57 +0530

    added the bin directory

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 const fortunes = require("./data/fortunes");
 
-const port = 3000;
-
 app.get("/fortunes", (req, res)=>{
     res.json(fortunes);
 })
 
-
-app.listen(port, ()=> console.log(`server is running at ${port}`));
+module.exports = app;
