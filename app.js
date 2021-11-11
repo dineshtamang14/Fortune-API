be14ac2a38abe9836266a7cef60d1837ab4b5404
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-11 23:23:31 +0530

    added the post routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const fortunes = require("./data/fortunes");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.get("/fortunes", (req, res)=>{
     res.json(fortunes);
@@ -13,5 +16,19 @@ app.get("/fortunes/random", (req, res)=>{
 app.get("/fortunes/:id", (req, res)=>{
     res.json(fortunes.find(fortune => fortune.id == req.params.id));
 });
+
+app.post("/fortunes", (req, res)=>{
+    console.log(req.body);
+
+    const { message, lucky_number, spirit_animal } = req.body;
+
+    const fortune_ids = fortunes.map(f=> f.id);
+
+    const fortune = {
+        id: (fortunes.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
+        message, lucky_number, 
+        spirit_animal 
+    };
+})
 
 module.exports = app;
